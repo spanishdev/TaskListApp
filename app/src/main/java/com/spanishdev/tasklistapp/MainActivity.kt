a8f2a8df6ea3e2cdcd7b0b3a77da90cdf73bc8e2
Jordi C <spanishdev@gmail.com>
2025-06-29 00:11:34 +0200

    Reinclude Scaffold

--- app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
+++ app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import com.spanishdev.tasklistapp.domain.repository.DebugTaskRepositoryImpl
 import com.spanishdev.tasklistapp.domain.usecase.GetTasksUseCase
 import com.spanishdev.tasklistapp.ui.theme.TaskListAppTheme
@@ -25,7 +29,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskListAppTheme {
-                TaskListScreen(viewModel = viewModel)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TaskListScreen(
+                        viewModel = viewModel,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
