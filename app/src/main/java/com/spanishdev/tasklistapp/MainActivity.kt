661dc4a92ffb1ba8428b82b8666a7014bd45e08e
Jordi C <spanishdev@gmail.com>
2025-06-28 20:24:32 +0200

    Add TaskListViewModelFactory

--- app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
+++ app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
@@ -5,14 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import com.spanishdev.tasklistapp.domain.repository.DebugTaskRepositoryImpl
+import com.spanishdev.tasklistapp.domain.usecase.GetTasksUseCase
 import com.spanishdev.tasklistapp.ui.theme.TaskListAppTheme
 import com.spanishdev.tasklistapp.ui.view.TaskListScreen
 import com.spanishdev.tasklistapp.ui.viewmodel.TaskListViewModel
+import com.spanishdev.tasklistapp.ui.viewmodel.TaskListViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel: TaskListViewModel by viewModels {
-        throw NotImplementedError("Later will be added a ViewModelProvider.Factory")
+        val repository = DebugTaskRepositoryImpl()
+        val dummyGetTasksUseCase = GetTasksUseCase(repository)
+        TaskListViewModelFactory(dummyGetTasksUseCase)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
