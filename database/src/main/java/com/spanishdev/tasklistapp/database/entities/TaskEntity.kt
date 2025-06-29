c6f9fc2d08f2f74ac67479cf15051648425aa2ae
Jordi C <spanishdev@gmail.com>
2025-06-29 02:12:41 +0200

    Add TaskEntity

--- database/src/main/java/com/spanishdev/tasklistapp/database/entities/TaskEntity.kt
+++ database/src/main/java/com/spanishdev/tasklistapp/database/entities/TaskEntity.kt
@@ -1,4 +1,14 @@
 package com.spanishdev.tasklistapp.database.entities
 
-class TaskEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val description: String,
+    val status: String,
+    val createdAt: Long = System.currentTimeMillis(),
+)
