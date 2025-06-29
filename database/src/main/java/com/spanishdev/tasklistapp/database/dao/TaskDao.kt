54f3b0d3f53e68dcd54701c18ab4b48b91d4b4d1
Jordi C <spanishdev@gmail.com>
2025-06-29 02:30:15 +0200

    Add TaskDao.kt

---
+++ database/src/main/java/com/spanishdev/tasklistapp/database/dao/TaskDao.kt
@@ -0,0 +1,52 @@
+package com.spanishdev.tasklistapp.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.spanishdev.tasklistapp.database.entities.TaskEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM tasks ORDER BY createdAt DESC")
+    fun getAllTasks(): Flow<List<TaskEntity>>
+
+
+    @Query("SELECT * FROM tasks ORDER BY createdAt ASC")
+    fun getAllTasksByCreatedAtAsc(): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM tasks ORDER BY name ASC")
+    fun getAllTasksByName(): Flow<List<TaskEntity>>
+
+    @Query("""
+        SELECT * FROM tasks 
+        ORDER BY
+                 CASE status
+                    WHEN 'PENDING' THEN 1
+                    WHEN 'IN_PROGRESS' THEN 2  
+                    WHEN 'DONE' THEN 3
+                    WHEN 'CANCELLED' THEN 4
+                    ELSE 5
+                END ASC, 
+                createdAt DESC
+    """)
+    fun getAllTasksByStatus(): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM tasks WHERE id = :id")
+    suspend fun getTaskById(id: Long): TaskEntity?
+
+    @Insert
+    suspend fun insertTask(taskEntity: TaskEntity): Long
+
+    @Update
+    suspend fun updateTask(taskEntity: TaskEntity)
+
+    @Delete
+    suspend fun deleteTask(taskEntity: TaskEntity)
+
+    @Query("DELETE FROM tasks")
+    suspend fun deleteAllTasks()
+}
