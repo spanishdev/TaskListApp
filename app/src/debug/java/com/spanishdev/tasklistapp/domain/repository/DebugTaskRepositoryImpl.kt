2193c236f16144ed096212c2986eeca6c6ae24af
Jordi C <spanishdev@gmail.com>
2025-06-30 00:42:19 +0200

    Update DebugTaskRepositoryImpl

--- app/src/debug/java/com/spanishdev/tasklistapp/domain/repository/DebugTaskRepositoryImpl.kt
+++ app/src/debug/java/com/spanishdev/tasklistapp/domain/repository/DebugTaskRepositoryImpl.kt
@@ -3,49 +3,53 @@ package com.spanishdev.tasklistapp.domain.repository
 import com.spanishdev.tasklistapp.domain.model.Status
 import com.spanishdev.tasklistapp.domain.model.Task
 
-class DebugTaskRepositoryImpl : com.spanishdev.tasklistapp.domain.repository.TaskRepository {
+class DebugTaskRepositoryImpl : TaskRepository {
 
-    private val taskList: MutableList<com.spanishdev.tasklistapp.domain.model.Task> = mutableListOf(
-        com.spanishdev.tasklistapp.domain.model.Task(
+    private val taskList: MutableList<Task> = mutableListOf(
+        Task(
             id = 1L,
             name = "Buy milk",
             description = "Go to supermarket and pickup 2 bottles",
-            status = com.spanishdev.tasklistapp.domain.model.Status.InProgress,
+            status = Status.InProgress,
         ),
-        com.spanishdev.tasklistapp.domain.model.Task(
+        Task(
             id = 2L,
             name = "Do chores",
             description = "Wash clothes and sweep the floor",
-            status = com.spanishdev.tasklistapp.domain.model.Status.Pending,
+            status = Status.Pending,
         ),
-        com.spanishdev.tasklistapp.domain.model.Task(
+        Task(
             id = 3L,
             name = "Prepare lunch",
             description = "Cook some nice rice",
-            status = com.spanishdev.tasklistapp.domain.model.Status.Pending,
+            status = Status.Pending,
         )
     )
 
-    override suspend fun addTask(task: com.spanishdev.tasklistapp.domain.model.Task) {
+    override suspend fun addTask(task: Task): Long {
         taskList.add(task)
+        return task.id
     }
 
-    override suspend fun deleteTask(task: com.spanishdev.tasklistapp.domain.model.Task) {
-        taskList.remove(task)
+    override suspend fun deleteTask(task: Task): Boolean {
+        return taskList.remove(task)
     }
 
-    override suspend fun updateTask(task: com.spanishdev.tasklistapp.domain.model.Task) {
+    override suspend fun updateTask(task: Task): Boolean {
         val index = taskList.indexOf(task)
-        if (index >= 0) {
+        return if (index >= 0) {
             taskList[index] = task
+            true
+        } else {
+            false
         }
     }
 
-    override suspend fun getTasks(): List<com.spanishdev.tasklistapp.domain.model.Task> {
+    override suspend fun getTasks(): List<Task> {
         return taskList
     }
 
-    override suspend fun getTaskById(id: Long): com.spanishdev.tasklistapp.domain.model.Task? {
+    override suspend fun getTaskById(id: Long): Task? {
         return taskList.find { it.id == id }
     }
 }
