dabd2ff49bb9e7afefd168396444567eaa183438
Jordi C <spanishdev@gmail.com>
2025-06-30 00:15:17 +0200

    Add UpdateTaskUseCase requirements

--- domain/src/main/java/com/spanishdev/tasklistapp/domain/usecase/UpdateTaskUseCase.kt
+++ domain/src/main/java/com/spanishdev/tasklistapp/domain/usecase/UpdateTaskUseCase.kt
@@ -2,7 +2,18 @@ package com.spanishdev.tasklistapp.domain.usecase
 
 import com.spanishdev.tasklistapp.domain.model.Task
 import com.spanishdev.tasklistapp.domain.repository.TaskRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class UpdateTaskUseCase(private val repository: TaskRepository) {
-    suspend operator fun invoke(task: Task) = repository.updateTask(task)
+class UpdateTaskUseCase(
+    private val repository: TaskRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+    suspend operator fun invoke(task: Task) = withContext(dispatcher) {
+        require(task.id > 0) { "Task id must be greater than 0" }
+        require(task.name.isNotBlank()) { "Task name cannot be blank" }
+
+        repository.updateTask(task)
+    }
 }
