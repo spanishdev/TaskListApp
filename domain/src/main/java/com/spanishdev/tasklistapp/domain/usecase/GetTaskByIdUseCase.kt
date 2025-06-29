9f8beb6264dadd073df6dfc5e4c42ca5a70cedec
Jordi C <spanishdev@gmail.com>
2025-06-29 23:05:44 +0200

    Add  GetTaskByIdUseCaseTest.kt

--- domain/src/main/java/com/spanishdev/tasklistapp/domain/usecase/GetTaskByIdUseCase.kt
+++ domain/src/main/java/com/spanishdev/tasklistapp/domain/usecase/GetTaskByIdUseCase.kt
@@ -1,7 +1,15 @@
 package com.spanishdev.tasklistapp.domain.usecase
 
 import com.spanishdev.tasklistapp.domain.repository.TaskRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class GetTaskByIdUseCase(private val repository: TaskRepository) {
-    suspend operator fun invoke(id: Long) = repository.getTaskById(id)
+class GetTaskByIdUseCase(
+    private val repository: TaskRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+    suspend operator fun invoke(id: Long) = withContext(dispatcher) {
+        repository.getTaskById(id)
+    }
 }
