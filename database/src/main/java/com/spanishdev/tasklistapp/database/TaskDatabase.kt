9e4abd2ebbc020ecdef1407ea0785fd8af288fb9
Jordi C <spanishdev@gmail.com>
2025-06-29 02:36:47 +0200

    Add TaskDatabase

--- database/src/main/java/com/spanishdev/tasklistapp/database/TaskDatabase.kt
+++ database/src/main/java/com/spanishdev/tasklistapp/database/TaskDatabase.kt
@@ -1,4 +1,21 @@
 package com.spanishdev.tasklistapp.database
 
-class TaskDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.spanishdev.tasklistapp.database.TaskDatabase.Companion.DATABASE_VERSION
+import com.spanishdev.tasklistapp.database.dao.TaskDao
+import com.spanishdev.tasklistapp.database.entities.TaskEntity
+
+@Database(
+    entities = [TaskEntity::class],
+    version = DATABASE_VERSION,
+    exportSchema = false,
+)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        const val DATABASE_NAME = "task_database"
+        const val DATABASE_VERSION = 1
+    }
 }
