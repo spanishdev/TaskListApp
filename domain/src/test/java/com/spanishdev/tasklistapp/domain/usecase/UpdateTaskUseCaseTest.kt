a124ee32289025fd778f957450061d16762a3f8c
Jordi C <spanishdev@gmail.com>
2025-06-30 00:56:54 +0200

    Add UpdateTaskUseCaseTest.kt

---
+++ domain/src/test/java/com/spanishdev/tasklistapp/domain/usecase/UpdateTaskUseCaseTest.kt
@@ -0,0 +1,67 @@
+package com.spanishdev.tasklistapp.domain.usecase
+
+import com.spanishdev.tasklistapp.domain.model.Status
+import com.spanishdev.tasklistapp.domain.model.Task
+import com.spanishdev.tasklistapp.domain.repository.TaskRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UpdateTaskUseCaseTest {
+
+    @Test
+    fun `WHEN Update Task success THEN returns true`() = runTest {
+        val repository = mockk<TaskRepository>()
+        val useCase = UpdateTaskUseCase(repository, UnconfinedTestDispatcher())
+        val taskToDelete = Task(45L, "Name", "Description", Status.Pending)
+
+        coEvery { repository.updateTask(any()) } returns true
+
+        val task = useCase(taskToDelete)
+
+        assertTrue(task)
+        coVerify { repository.updateTask(taskToDelete) }
+    }
+
+    @Test
+    fun `WHEN Update Task fail THEN returns false`() = runTest {
+        val repository = mockk<TaskRepository>()
+        val useCase = UpdateTaskUseCase(repository, UnconfinedTestDispatcher())
+        val taskToDelete = Task(45L, "Name", "Description", Status.Pending)
+
+        coEvery { repository.updateTask(any()) } returns false
+
+        val task = useCase(taskToDelete)
+
+        assertFalse(task)
+        coVerify { repository.updateTask(taskToDelete) }
+    }
+
+    @Test
+    fun `WHEN error thrown THEN raise an exception`() = runTest {
+        val repository = mockk<TaskRepository>()
+        val useCase = UpdateTaskUseCase(repository, UnconfinedTestDispatcher())
+        val expectedException = RuntimeException("Database error")
+        val taskToDelete = Task(45L, "Name", "Description", Status.Pending)
+
+        coEvery { repository.updateTask(any()) } throws expectedException
+
+        try {
+            useCase(taskToDelete)
+            fail("Expected RuntimeException")
+        } catch (e: RuntimeException) {
+            assertEquals(expectedException.message, e.message)
+        }
+
+        coVerify { repository.updateTask(any()) }
+    }
+}
