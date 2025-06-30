76269faa3160c55007490606174019ebcd351f75
Jordi C <spanishdev@gmail.com>
2025-06-30 19:35:24 +0200

    Add hilt to MainActivity

--- app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
+++ app/src/main/java/com/spanishdev/tasklistapp/MainActivity.kt
@@ -9,36 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.room.Room
-import com.spanishdev.tasklistapp.data.TaskRepositoryImpl
-import com.spanishdev.tasklistapp.database.TaskDatabase
-import com.spanishdev.tasklistapp.domain.repository.DebugTaskRepositoryImpl
-import com.spanishdev.tasklistapp.domain.usecase.GetTasksUseCase
 import com.spanishdev.tasklistapp.ui.theme.TaskListAppTheme
 import com.spanishdev.tasklistapp.ui.view.TaskListScreen
 import com.spanishdev.tasklistapp.ui.viewmodel.TaskListViewModel
-import com.spanishdev.tasklistapp.ui.viewmodel.TaskListViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val database by lazy {
-        Room.databaseBuilder(
-            applicationContext,
-            TaskDatabase::class.java,
-            TaskDatabase.DATABASE_NAME
-        ).build()
-    }
-
-    private val repository by lazy {
-       // TaskRepositoryImpl(database.taskDao())
-        DebugTaskRepositoryImpl()
-    }
-
-    private val viewModel: TaskListViewModel by viewModels {
-        val dummyGetTasksUseCase =
-            GetTasksUseCase(repository)
-        TaskListViewModelFactory(dummyGetTasksUseCase)
-    }
+    private val viewModel: TaskListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
