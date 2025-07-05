23be40a043ed29e94af6f1682a96d2c648146ba5
Jordi C <spanishdev@gmail.com>
2025-07-05 19:53:57 +0200

    Implement Paging on domain

--- app/src/main/java/com/spanishdev/tasklistapp/data/TaskRepositoryImpl.kt
+++ app/src/main/java/com/spanishdev/tasklistapp/data/TaskRepositoryImpl.kt
@@ -1,12 +1,19 @@
 package com.spanishdev.tasklistapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.map
 import com.spanishdev.tasklistapp.database.dao.TaskDao
+import com.spanishdev.tasklistapp.database.entities.TaskEntity
 import com.spanishdev.tasklistapp.domain.model.Task
 import com.spanishdev.tasklistapp.domain.repository.TaskRepository
 import com.spanishdev.tasklistapp.domain.repository.TaskRepository.TaskSort
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -30,18 +37,37 @@ class TaskRepositoryImpl @Inject constructor(
         return rowsAffected > 0
     }
 
-    override fun getTasks(sorting: TaskSort): Flow<List<Task>> {
-        val getTasksByOrder = when(sorting) {
-            TaskSort.CREATE_DATE -> taskDao.getAllTasksByCreatedAtAsc()
-            TaskSort.NAME -> taskDao.getAllTasksByName()
-            TaskSort.STATUS -> taskDao.getAllTasksByStatus()
+    override fun getTasks(sorting: TaskSort): Flow<PagingData<Task>> {
+        val pagingSourceFactory: () -> PagingSource<Int, TaskEntity> = when (sorting) {
+            TaskSort.CREATE_DATE -> {
+                { taskDao.getAllTasksByCreatedAtAsc() }
+            }
+
+            TaskSort.NAME -> {
+                { taskDao.getAllTasksByName() }
+            }
+
+            TaskSort.STATUS -> {
+                { taskDao.getAllTasksByStatus() }
+            }
         }
 
-        return getTasksByOrder.map { entities ->
-            entities.mapNotNull(taskMapper::toDomain)
-        }.flowOn(dispatcher)
+        return Pager(
+            config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+            .map { pagingData ->
+                pagingData.map { entity -> taskMapper.toDomain(entity) }
+            }
+            .filterNotNull()
+            .flowOn(dispatcher)
     }
 
     override suspend fun getTaskById(id: Long): Task? =
-        taskMapper.toDomain(taskDao.getTaskById(id))
+        taskDao.getTaskById(id)?.let(taskMapper::toDomain)
+
+
+    companion object {
+        const val PAGE_SIZE = 10
+    }
 }
